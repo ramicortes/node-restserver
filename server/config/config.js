4965a437ce76a39bd67c1b8f195cce44ca903b89
ramicortes <ramirocortes7@gmail.com>
2020-03-25 10:07:57 -0300

    REST Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // PUERTO
 // ========================
 process.env.PORT = process.env.PORT || 3000;
+
+// ========================
+// ENTORNO
+// ========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ========================
+// BASE DE DATOS
+// ========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://ramirocortes:t79NEeEyO7QX1qdz@cluster0-m3df7.mongodb.net/cafe?retryWrites=true&w=majority'
+}
+process.env.URLDB = urlDB;
